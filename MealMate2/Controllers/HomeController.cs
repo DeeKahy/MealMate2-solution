67c4dcfd694c5794fd6436fc424f7268a80d54db
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2023-06-30 19:18:51 +0200

    big update
    users work (it does not create a folder/databse entry for their inventory yet.
    inventory display

--- MealMate2/Controllers/HomeController.cs
+++ MealMate2/Controllers/HomeController.cs
@@ -13,12 +13,20 @@ namespace MealMate2.Controllers
             _logger = logger;
         }
 
+        // This could be your database context or repository
+        // where you fetch your data from.
+        // I'm just creating a new list for simplicity here.
         public IActionResult Index()
         {
-            return View();
+            string jsonFilePath = "C:/Users/Lenna/Desktop/Projects/MealMate2-solution/MealMate2/Data/USERS/" + User.Identity?.Name + "/Inventory/activeInventory.json";
+            return View(ActiveItemModel.LoadItemsFromJson(jsonFilePath));
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Inventory()
         {
             return View();
         }
---
+++ MealMate2/Models/ActiveItemModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace MealMate2.Models
+{
+    public class ActiveItemModel
+    {
+        // property for a food item, including name, experation date, and some other things        // property for a food item, including name, experation date, and some other things
+        public string Name { get; set; }
+        public string ExperationDate { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string weightORQuantity { get; set; }
+        public string initWeightORQuantity { get; set; }
+
+        // serialize the object to a string
+        public override string ToString() => JsonSerializer.Serialize<ActiveItemModel>(this);
+        public static List<ActiveItemModel> LoadItemsFromJson(string jsonFilePath)
+        {
+            string jsonData = File.ReadAllText(jsonFilePath);
+            var items = JsonSerializer.Deserialize<List<ActiveItemModel>>(jsonData);
+            return items;
+        }
+    }
+}
